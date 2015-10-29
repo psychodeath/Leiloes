d91377acd1570bda5e2bc40c78e778dbd8dc5485
João Mota <joao.lx@gmail.com>
2015-10-29 00:22:04 +0000

    tweaks - still not working - need to fix maven

---
+++ src/com/jm/Leiloes/GcsLeiloesWriter.java
@@ -0,0 +1,77 @@
+package com.jm.Leiloes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.channels.Channels;
+
+import com.google.appengine.tools.cloudstorage.GcsFileOptions;
+import com.google.appengine.tools.cloudstorage.GcsFilename;
+import com.google.appengine.tools.cloudstorage.GcsOutputChannel;
+import com.google.appengine.tools.cloudstorage.GcsService;
+import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
+import com.google.appengine.tools.cloudstorage.RetryParams;
+
+public class GcsLeiloesWriter {
+
+	public static final String BUCKETNAME = "scrapingbucket";
+    public static final String FILENAME = "resultsVendas.csv";
+    
+    private PrintWriter writer;
+    private GcsOutputChannel outputChannel;
+    
+	public GcsLeiloesWriter(){
+		outputChannel = null;
+		GcsFilename fileName = new GcsFilename(BUCKETNAME,FILENAME);
+
+        final GcsService gcsService = GcsServiceFactory.createGcsService(
+                new RetryParams.Builder()
+                    .initialRetryDelayMillis(10)
+                    .retryMaxAttempts(10)
+                    .totalRetryPeriodMillis(15000)
+                    .build()
+        );
+        
+        //GcsFileOptions.Builder gcsOptionsBuilder = new GcsFileOptions.Builder();
+        
+        //gcsOptionsBuilder.contentEncoding("ISO-8859-1");
+
+        try {
+            // TODO Fix character encoding on generated file
+            //GcsFileOptions gcsFileOptionsConfig = gcsOptionsBuilder.build();
+            outputChannel = gcsService.createOrReplace(fileName, GcsFileOptions.getDefaultInstance());
+
+        } catch (IOException e) {
+            System.out.println("something went wrong opening an output channel to the GCS service");
+            e.printStackTrace();
+        }
+
+        writer = new PrintWriter(Channels.newWriter(outputChannel, "ISO-8859-1"));
+        
+        writer.println("Tipologia;URL;Número;Preço base;Data limite;Serviço de Finanças;Estado;Modalidade");
+	}
+	
+	public void write(String txt){
+		this.writer.print(txt);
+	}
+	
+	public void writeln(){
+		this.writer.println();
+	}
+	
+	public void flush(){
+		this.writer.flush();
+	}
+	
+	public void close(){
+		this.writer.close();
+	}
+	
+	public void waitForWrites() throws IOException{
+		try{
+			outputChannel.waitForOutstandingWrites();
+		}
+		catch(IOException e){
+			throw e;
+		}
+	}
+}
--- src/com/jm/Leiloes/Scraper.java
+++ src/com/jm/Leiloes/Scraper.java
@@ -4,19 +4,12 @@ package com.jm.Leiloes;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
-import java.nio.channels.Channels;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import com.google.appengine.tools.cloudstorage.GcsFileOptions;
-import com.google.appengine.tools.cloudstorage.GcsFileOptions.Builder;
-import com.google.appengine.tools.cloudstorage.GcsService;
-import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
-import com.google.appengine.tools.cloudstorage.GcsFilename;
-import com.google.appengine.tools.cloudstorage.GcsOutputChannel;
-import com.google.appengine.tools.cloudstorage.RetryParams;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -37,11 +30,6 @@ import org.jsoup.select.Elements;
 @SuppressWarnings("serial")
 public class Scraper extends HttpServlet implements Serializable {
 
-    public static final String BUCKETNAME = "scrapingbucket";
-    public static final String FILENAME = "resultsVendas.csv";
-
-    //private DatastoreService dbs;
-
     // TODO code application logic here
     // http://www.e-financas.gov.pt/vendas/consultaVendasCurso.action?page=2&maximo=&concelho=&freguesia=&tipoBem=&dataMax=&dataMin=&distrito=&modalidade=&minimo=&tipoConsulta=XX
     // summary of changes/diff with previous version of csv
@@ -54,39 +42,37 @@ public class Scraper extends HttpServlet implements Serializable {
     //TESTED:
     //FBConnector facebook = new FBConnector();
 
-    public String parse(){
+	private String WriteLeilaoToGcs(LinkedList<Leilao> leiloes){
+		GcsLeiloesWriter glw = new GcsLeiloesWriter();
+		
+		for(Leilao l : leiloes){
+			System.out.println("datastore: leilão " + l.getId() + " com tipologia " + l.getTipologia());
+			
+			glw.write(l.getTipologia() + ";");
+			glw.write(l.getUrl() + ";");
+			glw.writeln();
+            glw.flush();
+            glw.close();
+            
+            OfyService.ofy().save().entity(l).now();
+            System.out.println("saving to datastore complete");
+		}
+		
+        try {            
+            glw.waitForWrites();
+            System.out.println("finished writing file to GCS");
 
-        LinkedList<Leilao> leiloes = new LinkedList<>();
-
-        final GcsService gcsService = GcsServiceFactory.createGcsService(
-                new RetryParams.Builder()
-                    .initialRetryDelayMillis(10)
-                    .retryMaxAttempts(10)
-                    .totalRetryPeriodMillis(15000)
-                    .build()
-        );
-
-        GcsFilename fileName = new GcsFilename(BUCKETNAME,FILENAME);
-        
-        //dbs = DatastoreServiceFactory.getDatastoreService();    // init objectify datastore service
-
-        GcsOutputChannel outputChannel = null;
-        GcsFileOptions.Builder gcsOptionsBuilder = new GcsFileOptions.Builder();
-        
-        gcsOptionsBuilder.contentEncoding("ISO-8859-1");
-
-
-        try {
-            // TODO Fix character encoding on generated file
-            GcsFileOptions gcsFileOptionsConfig = gcsOptionsBuilder.build();
-            outputChannel = gcsService.createOrReplace(fileName, gcsFileOptionsConfig);
-
+            return "Tamanho da lista de leilões: " + leiloes.size();
         } catch (IOException e) {
-            System.out.println("something went wrong opening an output channel to the GCS service");
+            System.out.println("something went wrong finalizing writes to output channel");
             e.printStackTrace();
         }
 
-        PrintWriter writer = new PrintWriter(Channels.newWriter(outputChannel, "ISO-8859-1"));
+        return "commit to gcs exited abnormally";
+	}
+	
+    private String parse(){
+        LinkedList<Leilao> leiloes = new LinkedList<>();
 
         try {
             Document doc;
@@ -95,7 +81,6 @@ public class Scraper extends HttpServlet implements Serializable {
 
             // PrintWriter writer = new PrintWriter("file", "ISO-8859-1");
 
-            writer.println("Tipologia;URL;Número;Preço base;Data limite;Serviço de Finanças;Estado;Modalidade");
 
             String urlToOpen = "http://www.e-financas.gov.pt/vendas/consultaVendasCurso.action?tipoConsulta=02&modalidade=&distrito=&concelho=&minimo=++.+++.+++.+++%2C++&maximo=++.+++.+++.+++%2C++&dataMin=&dataMax=";
 
@@ -126,31 +111,31 @@ public class Scraper extends HttpServlet implements Serializable {
 
                     Elements tipologia = e.getElementsByClass("info-table-title");
                     if (tipologia.size() > 1){
-                        writer.print(tipologia.get(1).text() + ";");
 
                         leilao.setTipologia(tipologia.get(1).text());
                         String url = tipologia.get(2).getElementsByAttribute("href").first().attr("abs:href");
-                        writer.print(url + ";");
+                        
                         leilao.setUrl(url);
+                    }
+                    else{
+                    	leilao.setTipologia("n/a");
+                    	leilao.setUrl("n/a");
                     }
 
                     for (Element imovel : e.getElementsByClass("info-element")){
                         found = true;
                         ArrayList<String> resto = new ArrayList<String>();
                         for (Element infoText : imovel.getElementsByClass("info-element-text")){
-                            writer.print(infoText.text() + ";");
+                            //glw.write(infoText.text() + ";");
                             resto.add(infoText.text());
                         }
-                        // TODO acrescentar resto à DataStore, ajustar parsing morada (tamanho variável)
+                        // TODO acrescentar resto à bd, ajustar parsing morada (tamanho variável)
 
                     }
 
                     if (found){
-                        writer.println();
-                        //dbs.put(leilao);
-
                         try {
-                            OfyService.ofy().save().entity(leilao).now();
+                            //OfyService.ofy().save().entity(leilao).now();
                             leiloes.add(leilao);
                         } catch (Exception e1) {
                             System.out.println("persistence to datastore failed");
@@ -162,51 +147,16 @@ public class Scraper extends HttpServlet implements Serializable {
 
                 Element el = doc.getElementsByAttributeValueStarting("href","consultaVendasCurso").last();
 
-                //System.out.println("último elemento identificado com: " + el.attr("href"));
-
-
-
                 urlToOpen = el.attr("abs:href");
 
             } while((doc.hasText()) && (!beyondEnd));
-            writer.flush();
-            writer.close();
-            //dbs.put(leilao);
-
         } catch (Exception ex) {
             System.out.println("Exception: " + ex.getMessage());
             Logger.getLogger(Scraper.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                outputChannel.waitForOutstandingWrites();
-
-                //outputChannel.close();
-
-                System.out.println("finished writing to GCS");
-
-                for(Leilao l : leiloes){
-                    System.out.println("datastore: leilão " + l.getId() + " com tipologia " + l.getTipologia());
-                    //OfyService.factory().begin();
-                    OfyService.ofy().save().entity(l).now();
-                }
-                System.out.println("saving to datastore complete");
-
-/*                ListIterator<Leilao> iterLeiloes = leiloes.listIterator();
-                while(iterLeiloes.hasNext()){
-                    System.out.print("*");
-                    OfyService.ofy().save().entity(iterLeiloes.next()).now();
-                }*/
-
-
-                return "Tamanho da lista de leilões: " + leiloes.size();
-
-
-            } catch (IOException e) {
-                System.out.println("something went wrong finalizing writes to output channel");
-                e.printStackTrace();
-            }
-
         }
+        
+        this.WriteLeilaoToGcs(leiloes);
+        
         return "parsing function failed";
     }
 
@@ -217,45 +167,13 @@ public class Scraper extends HttpServlet implements Serializable {
         response.setCharacterEncoding("UTF-8");
 
         PrintWriter writer = response.getWriter();
-        writer.write("event:status_change\n");
-        writer.write("data:parser initiated\n\n");
-        writer.flush();
 
         writer.write(this.parse() + "\n");
 
-        writer.write("event:status_change\n");
-        writer.write("data:parser completed\n\n");
         writer.flush();
         writer.close();
 
         System.out.println("write operation complete.");
-
-        //PrintWriter out = response.getWriter();
-
-        /*int count = 0;
-
-        ThreadFactory tf = ThreadManager.currentRequestThreadFactory();
-
-        Thread t = tf.newThread(ScraperVendasFinancas());
-
-        t.start();
-        System.out.println("Parser thread running.");
-
-        while(t.isAlive()){
-            request.setAttribute("scrapeCounter", count++);
-            request.setAttribute("testCount", count);
-
-            this.getServletContext().getRequestDispatcher("/index.jsp").forward(request,response);
-            System.out.println("Hit: " + count);
-        }
-
-        request.setAttribute("runningParser", t);
-        this.getServletContext().getRequestDispatcher("/index.jsp").forward(request,response);
-        System.out.println("pushed thread object back into JSP");
-
-        System.out.println();
-        System.out.println("Finished.");*/
-        //return true;
 
     }
 
